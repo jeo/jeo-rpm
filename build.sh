ad51c9f49f16b8b0277f9846b7958ba3aa4fedb8
Justin Deoliveira <jdeolive@gmail.com>
2015-03-03 23:43:03 +0000

    explicit naming of output file on wget call

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -e
 
 arch=`uname -m`
 dist=`cat /etc/rpm/macros.dist | grep "%dist" | cut -f 2 -d ' ' | sed 's/^\.//g'`
-
 # set up some variables
 pushd `dirname $0`
 build_root=`pwd`/rpmbuild
@@ -16,12 +15,15 @@ rpm_build_dir=$rpm_build_root/BUILD
 
 # figure out the version
 ver=`cat $build_root/SPECS/jeo.spec | grep "Version:" | sed 's/Version: *//g'`
+jeo_zip=jeo-$ver-cli.zip
+gdal_tgz=gdaljni-1.9.2-el6-${arch}.tgz
+
 
 # grab the sources
 pushd $build_root/SOURCES
 set +e; rm *; set -e
-wget https://github.com/jeo/jeo-cli/releases/download/$ver/jeo-$ver-cli.zip
-wget https://github.com/jeo/jeo-cli/releases/download/$ver/gdaljni-1.9.2-el6-${arch}.tgz
+wget -O $jeo_zip https://github.com/jeo/jeo-cli/releases/download/$ver/$jeo_zip
+wget -O $gdal_tgz https://github.com/jeo/jeo-cli/releases/download/$ver/$gdal_tgz
 popd
 
 # clean out the build root
